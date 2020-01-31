831857651f1b9926060b1bf0186f298844031c74
savenger <sascha.mahmood@nextragen.de>
2020-02-01 00:29:07 +0100

    added Tileset object (without correct image)

--- PipeArea.gd
+++ PipeArea.gd
@@ -5,19 +5,31 @@ export (int) var speed = 950
 var target = Vector2()
 var velocity = Vector2()
 
+func _ready():
+	print("----")
+	print(target)
+	print(global_position)
+	target = global_position
+	print("####")
+	print(target)
+	print(global_position)
+	print("----")
+
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_RIGHT \
 	and event.is_pressed():
-		rotate(PI/2.0)
+		if event.button_index == BUTTON_RIGHT:
+			rotate(PI/2.0)
+		if event.button_index == BUTTON_LEFT:
+			get_parent().globals.selected = self
 
 func _input(event):
-	if event.is_action_pressed('click'):
-		target = get_global_mouse_position()
+	if get_parent().globals.selected == self:
+		if event.is_action_pressed('click'):
+			target = get_global_mouse_position()
 
 func _physics_process(delta):
-	velocity = position.direction_to(target) * speed
+	velocity = global_position.direction_to(target) * speed
 	# look_at(target)
-	if position.distance_to(target) > 10:
-		position += velocity * delta
-	print(position)
+	if global_position.distance_to(target) > 10:
+		global_position += velocity * delta
--- Sink.gd
+++ Sink.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+class Globals:
+	var selected = null
+
+var active_inv_item = null
+var globals = Globals.new()
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,8 +13,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# get_node("GridContainer/Pipe2").draw_set_transform(Vector2(200, 50), 0.0, Vector2(1,1))
-	pass
+	var Pipe = load("res://Pipe.tscn")
+	var inventory = [1,2,3,4,5]
+	for i in inventory:
+		var node = Pipe.instance()
+		node.position.x = 150 * i
+		node.position.y = 400
+		node.globals = globals
+		add_child(node)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
