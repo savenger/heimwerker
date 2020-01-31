45bdc89091edb66b9da1c26af0fd2e03bce08afb
savenger <sascha.mahmood@nextragen.de>
2020-01-31 19:46:20 +0100

    Moving Sprite upon mouse click

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,24 +1,9 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-export (float) var rotation_speed = 1.5
-
-var velocity = Vector2()
-var rotation_dir = 0
-
-func get_input():
-	rotation_dir = 0
-	velocity = Vector2()
-	if Input.is_action_pressed('ui_right'):
-		rotation_dir += 1
-	if Input.is_action_pressed('ui_left'):
-		rotation_dir -= 1
-	if Input.is_action_pressed('ui_down'):
-		velocity = Vector2(-speed, 0).rotated(rotation)
-	if Input.is_action_pressed('ui_up'):
-		velocity = Vector2(speed, 0).rotated(rotation)
+export (int) var speed = 950
 
 var target = Vector2()
+var velocity = Vector2()
 
 func _input(event):
 	if event.is_action_pressed('click'):
@@ -27,10 +12,5 @@ func _input(event):
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
 	# look_at(target)
-
-func _physics_process(delta):
-	get_input()
-	rotation += rotation_dir * rotation_speed * delta
-	velocity = move_and_slide(velocity)
-	if position.distance_to(target) > 5:
+	if position.distance_to(target) > 10:
 		velocity = move_and_slide(velocity)
