aa4cc9a5e145d368f1006a957de2d9cc8ec62f5f
savenger <sascha.mahmood@nextragen.de>
2020-01-31 23:23:43 +0100

    added Rotation

--- PipeArea.gd
+++ PipeArea.gd
@@ -1,7 +1,23 @@
 extends Area2D
 
+export (int) var speed = 950
+
+var target = Vector2()
+var velocity = Vector2()
+
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_RIGHT \
 	and event.is_pressed():
 		rotate(PI/2.0)
+
+func _input(event):
+	if event.is_action_pressed('click'):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		position += velocity * delta
+	print(position)
