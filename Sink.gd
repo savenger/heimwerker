bd21278848ad1350b85453633a85e14ecb429e13
savenger <sascha.mahmood@nextragen.de>
2020-01-31 23:11:58 +0100

    improvements

---
+++ Sink.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# get_node("GridContainer/Pipe2").draw_set_transform(Vector2(200, 50), 0.0, Vector2(1,1))
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
