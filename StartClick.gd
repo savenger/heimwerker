a40d2e4fc4bf220bb2c8255141451014d2ee98d6
savenger <sascha.mahmood@nextragen.de>
2020-02-02 13:23:07 +0100

    mouse hover on start images

--- StartClick.gd
+++ StartClick.gd
@@ -8,11 +8,17 @@ func _ready():
 	pass
 
 func _input_event(_viewport, event, _shape_idx):
+	var img = get_parent().get_node("ActionImage")
 	# print(viewport, shape_idx)
-	if event is InputEventMouseButton \
-	and event.button_index == BUTTON_LEFT \
-	and event.is_pressed():
-		self.on_click()
+	if event is InputEventMouseMotion:
+		if img:
+			img.visible = true
+			img.position = self.position
+	if event is InputEventMouseButton:
+		# set_default_cursor_shape(Control.CURSOR_POINTING_HAND)
+		if event.button_index == BUTTON_LEFT \
+		and event.is_pressed():
+			self.on_click()
 
 func on_click():
 	# print("res://scenes/"+self.name+".tscn")
@@ -21,3 +27,9 @@ func on_click():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_mouse_exited():
+	var img = get_parent().get_node("ActionImage")
+	if img:
+		img.visible = false
